5c658c0797b58ecffd473712ed65138ebc9edf05
Ahmed-Fa1sal <DerRiese765@gmail.com>
2025-07-17 15:45:13 +0300

    Cart saved to local storage

--- src/app/services/cart.ts
+++ src/app/services/cart.ts
@@ -6,16 +6,27 @@ import { Injectable } from '@angular/core';
 export class CartService {
   private cart: any[] = [];
 
+  constructor(){
+    const storedCart = localStorage.getItem('cart');
+    if(storedCart){
+      this.cart = JSON.parse(storedCart);
+    }
+  }
   getCart(){
     return this.cart;
   }
   
   addToCart(product: any){
     this.cart.push(product);
+    this.saveCart();
   }
 
   removeFromCart(productToRemove: any){
     this.cart = this.cart.filter(item => item !== productToRemove);
 
+  }
+
+  private saveCart(){
+    localStorage.setItem('cart', JSON.stringify(this.cart));
   }
 }
